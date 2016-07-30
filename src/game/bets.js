d06cd210cca27d4236ba418c93e3ca50d8a23895
aedm <>
2016-07-30 02:57:32 +0200

    Added characters and events.

--- src/game/bets.js
+++ src/game/bets.js
@@ -1,10 +1,362 @@
-export const Bets = {
-  "cersei": {
+export const ThreePointCharacters = [
+  {
+    token: "tyrion",
+    points: 3,
+    name: "Tyrion Lannister",
+  },
+  {
+    token: "cersei",
     points: 3,
     name: "Cersei Lannister",
   },
-  "jaime": {
+  {
+    token: "jaime",
     points: 3,
-    name: "Jaime Lannister",
+    name: "JaimeLannister",
   },
-};
+  {
+    token: "jonsnow",
+    points: 3,
+    name: "Jon Snow",
+  },
+  {
+    token: "sansa",
+    points: 3,
+    name: "Sansa Stark",
+  },
+  {
+    token: "arya",
+    points: 3,
+    name: "Arya Stark",
+  },
+  {
+    token: "bran",
+    points: 3,
+    name: "Bran Stark",
+  },
+  {
+    token: "daenerys",
+    points: 3,
+    name: "Daenerys Targaryen",
+  },
+  {
+    token: "varys",
+    points: 3,
+    name: "Lord Varys",
+  },
+  {
+    token: "baelish",
+    points: 3,
+    name: "Petyr Baelish",
+  },
+  {
+    token: "theon",
+    points: 3,
+    name: "Theon Greyjoy",
+  },
+  {
+    token: "olenna",
+    points: 3,
+    name: "Olenna Tyrell",
+  },
+  {
+    token: "ellaria",
+    points: 3,
+    name: "Ellaria Sand",
+  },
+  {
+    token: "nightking",
+    points: 3,
+    name: "The Night King",
+  },
+];
+
+
+export const TwoPointCharacters = [
+  {
+    token: "jorah",
+    points: 2,
+    name: "Jorah Mormont",
+  },
+  {
+    token: "greyworm",
+    points: 2,
+    name: "Grey Worm",
+  },
+  {
+    token: "dragon",
+    points: 2,
+    name: "Any dragon",
+  },
+  {
+    token: "hound",
+    points: 2,
+    name: "Sandor Clegane",
+  },
+  {
+    token: "mountain",
+    points: 2,
+    name: "Gregor Clegane",
+  },
+  {
+    token: "bronn",
+    points: 2,
+    name: "Bronn",
+  },
+  {
+    token: "asha",
+    points: 2,
+    name: "Asha Greyjoy",
+  },
+  {
+    token: "euron",
+    points: 2,
+    name: "Euron Greyjoy",
+  },
+  {
+    token: "sam",
+    points: 2,
+    name: "Samwell Tarly",
+  },
+  {
+    token: "ghost",
+    points: 2,
+    name: "Ghost",
+  },
+  {
+    token: "tollett",
+    points: 2,
+    name: "Eddison Tollett",
+  },
+  {
+    token: "tormund",
+    points: 2,
+    name: "Tormund Giantsbane",
+  },
+  {
+    token: "serdavos",
+    points: 2,
+    name: "Ser Davos Seaworth",
+  },
+  {
+    token: "brienne",
+    points: 2,
+    name: "Brienne of Tarth",
+  },
+  {
+    token: "meera",
+    points: 2,
+    name: "Meera Reed",
+  },
+  {
+    token: "tyene",
+    points: 2,
+    name: "Tyene Sand",
+  },
+];
+
+export const OnePointCharacters = [
+  {
+    token: "benjen",
+    points: 1,
+    name: "Benjen Stark",
+  },
+  {
+    token: "missandei",
+    points: 1,
+    name: "Missandei",
+  },
+  {
+    token: "daario",
+    points: 1,
+    name: "Daario Naharis",
+  },
+  {
+    token: "melisandre",
+    points: 1,
+    name: "Melisandre",
+  },
+  {
+    token: "jaqen",
+    points: 1,
+    name: "Jaqen H'ghar",
+  },
+  {
+    token: "podrick",
+    points: 1,
+    name: "Podrick Payne",
+  },
+  {
+    token: "gilly",
+    points: 1,
+    name: "Gilly",
+  },
+  {
+    token: "beric",
+    points: 1,
+    name: "Beric Dondarrion",
+  },
+  {
+    token: "thoros",
+    points: 1,
+    name: "Thoros of Myr",
+  },
+  {
+    token: "lyanna",
+    points: 1,
+    name: "Lyanna Mormont",
+  },
+  {
+    token: "edmure",
+    points: 1,
+    name: "Edmure Tully",
+  },
+  {
+    token: "obara",
+    points: 1,
+    name: "Obara Sand",
+  },
+  {
+    token: "nymeria",
+    points: 1,
+    name: "Nymeria Sand",
+  },
+  {
+    token: "qyburn",
+    points: 1,
+    name: "Qyburn",
+  },
+  {
+    token: "robin",
+    points: 1,
+    name: "Robin Arryn",
+  },
+  {
+    token: "randyll",
+    points: 1,
+    name: "Randyll Tarly",
+  },
+  {
+    token: "gendry",
+    points: 1,
+    name: "Gendry",
+  },
+];
+
+export const TwoPointEvents = [
+  {
+    token: "jon-learns-targaryen",
+    points: 2,
+    name: "Jon Snow learns he's a Targaryen",
+  },
+  {
+    token: "hodor-white-walker",
+    points: 2,
+    name: "Hodor becomes a White Walker",
+  },
+  {
+    token: "greyworm-fights-mountain",
+    points: 2,
+    name: "Grey Worm fights The Mountain",
+  },
+  {
+    token: "cleganebowl",
+    points: 2,
+    name: "Cleganebowl! The Hound fights The Mountain",
+  },
+  {
+    token: "dragon-kills-cersei",
+    points: 2,
+    name: "A dragon kills Cersei",
+  },
+  {
+    token: "jaime-kills-cersei",
+    points: 2,
+    name: "Jaime kills Cersei",
+  },
+  {
+    token: "arya-kills-three",
+    points: 2,
+    name: "Arya kills more than two people",
+  },
+  {
+    token: "bran-warg",
+    points: 2,
+    name: "Bran wargs into someone on this list",
+  },
+  {
+    token: "jon-twin",
+    points: 2,
+    name: "Jon Snow has a twin",
+  },
+  {
+    token: "jon-kiss-daenerys",
+    points: 2,
+    name: "Jon Snow kisses Daenerys",
+  },
+  {
+    token: "white-cross",
+    points: 2,
+    name: "A White Walker crosses the wall",
+  },
+  {
+    token: "wall-falls",
+    points: 2,
+    name: "The wall falls down",
+  },
+  {
+    token: "sam-fights-sword",
+    points: 2,
+    name: "Sam fights using his father's sword",
+  },
+  {
+    token: "catelyn-lives",
+    points: 2,
+    name: "Catelyn Stark is alive",
+  },
+  {
+    token: "nymeria-arya",
+    points: 2,
+    name: "Arya meets Nymeria",
+  },
+  {
+    token: "gendry-back",
+    points: 2,
+    name: "Gendry is back",
+  },
+  {
+    token: "dragon-ride",
+    points: 2,
+    name: "Someone who's not Daenerys rides a dragon",
+  },
+  {
+    token: "greyscale-heals",
+    points: 2,
+    name: "Jorah Mormont's greyscale heals/stops",
+  },
+  {
+    token: "mad-queen",
+    points: 2,
+    name: "Someone calls Cersei 'The Mad Queen'",
+  },
+  {
+    token: "sansa-marry-littlefinger",
+    points: 2,
+    name: "Sansa marries Littlefinger",
+  },
+  {
+    token: "stark-reunion",
+    points: 2,
+    name: "All remaining Starks at one place",
+  },
+];
+
+
+export const Bets = {};
+
+Meteor.startup(function () {
+  ThreePointCharacters.forEach(x => Bets[x.token] = x);
+  TwoPointCharacters.forEach(x => Bets[x.token] = x);
+  OnePointCharacters.forEach(x => Bets[x.token] = x);
+  TwoPointEvents.forEach(x => Bets[x.token] = x);
+});
