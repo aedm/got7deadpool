435d4310215c1c060f70fc0eb59b1a5fb608f927
aedm <>
2016-08-11 03:38:59 +0200

    Style changes.

--- src/client/components/game-page/game-page.jsx
+++ src/client/components/game-page/game-page.jsx
@@ -23,47 +23,43 @@ export class _GamePage extends React.Component {
 
   renderRules() {
     return <div className="container">
-      <div className="row rules row-eq-height">
-        <div className="text-center col-md-5 col-md-offset-1">
-          <div>
-            <h2>What's this game about?</h2>
-            <p>Predict who dies in season 7 of Game of Thrones
-              and compete against others.</p>
-            <p>Voting is closed right before the season starts. Scores get updated after each
-              episode. Your predictions are public.</p>
-            <p>It's for free and you can't win anything.</p>
-            <p>This is a fan-made website, I have no affiliation with HBO, everything here is the
-              property of their respective owners.</p>
-          </div>
+      <div className="gameinfo">
+        <div className="gameinfo-box">
+          <h2>What's this game about?</h2>
+          <p>Predict who dies in season 7 of Game of Thrones
+            and compete against others.</p>
+          <p>Voting is closed right before the season starts. Scores get updated after each
+            episode. Your predictions are public.</p>
+          <p>It's for free and you can't win anything.</p>
+          <p>This is a fan-made website, I have no affiliation with HBO, everything here is the
+            property of their respective owners.</p>
         </div>
-        <div className="text-center col-md-5">
-          <div>
-            <h2>Scoring</h2>
-            <p>Scores in this chart will get multiplied by the value of each character.</p>
-            <Table className="rule-table">
-              <tbody>
-              <tr>
-                <td className="rules-empty"/>
-                <td colSpan="2">Your prediction</td>
-              </tr>
-              <tr>
-                <td className="rules-empty"/>
-                <td>Dies</td>
-                <td>Survives</td>
-              </tr>
-              <tr>
-                <td>Dies in the show</td>
-                <td>2</td>
-                <td>-1</td>
-              </tr>
-              <tr>
-                <td>Survives in the show</td>
-                <td>0</td>
-                <td>1</td>
-              </tr>
-              </tbody>
-            </Table>
-          </div>
+        <div className="gameinfo-box">
+          <h2>Scoring</h2>
+          <p>Scores in this chart will get multiplied by the value of each character.</p>
+          <Table className="rule-table">
+            <tbody>
+            <tr>
+              <td className="rules-empty"/>
+              <td colSpan="2">Your prediction</td>
+            </tr>
+            <tr>
+              <td className="rules-empty"/>
+              <td>Dies</td>
+              <td>Survives</td>
+            </tr>
+            <tr>
+              <td>Dies in the show</td>
+              <td>2</td>
+              <td>-1</td>
+            </tr>
+            <tr>
+              <td>Survives in the show</td>
+              <td>0</td>
+              <td>1</td>
+            </tr>
+            </tbody>
+          </Table>
         </div>
       </div>
     </div>;
