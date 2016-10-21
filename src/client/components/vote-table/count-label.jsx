2ec599d024599785ad674bf8e7b588ef59ffea76
aedm <>
2016-10-22 00:40:38 +0200

    Updates table when maximum color range changes.

---
+++ src/client/components/vote-table/count-label.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+export class CountLabel extends React.Component {
+  // Generates background color for the "sum" cell
+  getBackgroundColor() {
+    let ratio = this.props.voteCount / this.props.maxVoteCount;
+    // 120 is green, 0 is red
+    let hue = 120 * (1 - ratio);
+    let saturation = 0 + 100 * ratio;
+    let lightness = 30 + 30 * ratio;
+    return `hsl(${hue},${saturation}%,${lightness}%)`;
+  }
+
+  render() {
+    let background = this.getBackgroundColor();
+    return <div className="votetable-count">
+      <div className="votetable-count-label" style={{"backgroundColor": background}}>
+        {this.props.voteCount}
+      </div>
+    </div>;
+  }
+}
+
+CountLabel.propTypes = {
+  // Sum of all counts for this bet
+  voteCount: React.PropTypes.number,
+
+  // The number of votes on the most popular bet
+  maxVoteCount: React.PropTypes.number,
+};
--- src/client/components/vote-table/vote-table-row.jsx
+++ src/client/components/vote-table/vote-table-row.jsx
@@ -1,8 +1,9 @@
 import {Meteor} from 'meteor/meteor';
 import React from 'react';
-import {OverlayTrigger, Tooltip} from 'react-bootstrap';
 
 import {CustomCheckbox} from '/src/client/components/custom-checkbox/custom-checkbox.jsx';
+import {CountLabel} from '/src/client/components/vote-table/count-label.jsx';
+import {FriendsList} from '/src/client/components/vote-table/friends-list.jsx';
 
 
 export class VoteTableRow extends React.Component {
@@ -20,18 +21,6 @@ export class VoteTableRow extends React.Component {
     });
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    if (nextState.vote !== this.state.vote
-        || nextProps.voteCount != this.props.voteCount
-        || nextProps.votingPlayers.length != this.props.votingPlayers.length
-        || !nextProps.votingPlayers.every((x, i) => x === this.props.votingPlayers[i])) return true;
-
-    // TODO: this is bug, but if the max vote changes, updating everyting is really slow. See #7
-    // if (nextProps.maxVoteCount != this.props.maxVoteCount) return true;
-
-    return false;
-  }
-
   handleToggle() {
     let newState = !this.state.vote;
     this.setState({vote: newState});
@@ -39,48 +28,7 @@ export class VoteTableRow extends React.Component {
     setTimeout(() => Meteor.call("player/bet", this.props.bet.token, newState), 0);
   }
 
-  // Generates background color for the "sum" cell
-  getBackgroundColor() {
-    let ratio = this.props.voteCount / this.props.maxVoteCount;
-    // 120 is green, 0 is red
-    let hue = 120 * (1 - ratio);
-    let saturation = 0 + 100 * ratio;
-    let lightness = 30 + 30 * ratio;
-    return `hsl(${hue},${saturation}%,${lightness}%)`;
-  }
-
-  renderFriendsList() {
-    if (!this.props.player) return null;
-
-    let vote = this.props.player.vote;
-
-    return <div className="votetable-friends-list">
-      <OverlayTrigger placement="top" overlay={<Tooltip id="tooltip">You</Tooltip>}>
-        <img className={`votetable-avatar votetable-avatar-self ${vote ? "votetable-voted" : ""}`}
-             style={{zIndex: this.props.votingPlayers.length + 1}}
-             src={this.props.user.profile.photo || "/asset/avatar50px.jpg"}/>
-      </OverlayTrigger>
-      { this.props.votingPlayers.map((player, index) =>
-          <OverlayTrigger key={index} placement="top"
-                          overlay={<Tooltip id="tooltip">{player.profile.name}</Tooltip>}>
-            <img className="votetable-avatar"
-                 style={{zIndex: `${this.props.votingPlayers.length - index}`}}
-                 src={player.profile.photo || "/asset/avatar50px.jpg"}/>
-          </OverlayTrigger>)}
-    </div>;
-  }
-
   render() {
-    let voteCount = null;
-    if (this.props.voteCount >= 0) {
-      let background = this.getBackgroundColor();
-      voteCount = <div className="votetable-count">
-        <div className="votetable-count-label" style={{"backgroundColor": background}}>
-          {this.props.voteCount}
-        </div>
-      </div>;
-    }
-
     let voteCheckbox = null;
     if (this.props.player) {
       voteCheckbox = <div className="votetable-checkbox-cell">
@@ -95,9 +43,11 @@ export class VoteTableRow extends React.Component {
     }
 
     return <div className="votetable-vote" key={this.props.token}>
-      { this.renderFriendsList() }
+      { !this.props.player ? null :
+          <FriendsList player={this.props.player} votingPlayers={this.props.votingPlayers}
+                       user={this.props.user}/> }
       <div className="votetable-row">
-        { voteCount }
+        <CountLabel voteCount={this.props.voteCount} maxVoteCount={this.props.maxVoteCount}/>
         { avatar }
         <div className="votetable-name">{this.props.bet.name}</div>
         { voteCheckbox }
